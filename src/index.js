b208b73baae40aeb2d7609d2c5e8b4a58373b0b2
Stefan LENTNER <slentner@KLEE.LAN.NET>
2017-07-21 14:27:25 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -4,31 +4,14 @@ var SlackBot = require('slackbots');
 
 // create a bot
 var bot = new SlackBot({
-    token: 'xoxb-215197751904-gMnLEDLSPUD8d0DvwyS5B2AV', // to be replaced
+    token: 'REPLACE_ME', // replace this by the token you'll find at {ourslackAdress}+/services/
     name: 'mdap-sky-net'
 });
 
 bot.on('start', function() {
     // more information about additional params https://api.slack.com/methods/chat.postMessage
     var params = {
-        icon_emoji: ':cat:'
     };
-    
-    // // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-    // bot.postMessageToChannel('general', 'meow!', params);
-    
-    // define existing username instead of 'user_name'
-    bot.postMessageToUser('paul-antoine', 'le slackbot est mort, vive le slackbot!', params); 
-
-    bot.postMessageToUser('paul-antoine', 'Plus rien de pourra arrêter ce bot!', params); 
-
+    // TODO// Améliorer la dev experience
     bot.postMessageToUser('stefan.lentner', 'le slackbot est mort, vive le slackbot!', params); 
-
-    bot.postMessageToUser('stefan.lentner', 'Plus rien de pourra arrêter ce bot!', params); 
-    // If you add a 'slackbot' property, 
-    // you will post to another user's slackbot channel instead of a direct message
-    // bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
-    
-    // define private group instead of 'private_group', where bot exist
-    // bot.postMessageToGroup('private_group', 'meow!', params); 
 });
